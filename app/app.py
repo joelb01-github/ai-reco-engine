1f58226afbf928d9809bb154d92407caaeca3d2e
Joel Barenco <joelbarenco@gmail.com>
2020-10-02 15:00:20 +0200

    updated ports to 5001

--- app/app.py
+++ app/app.py
@@ -17,4 +17,4 @@ def reco():
   return json.dumps(recommendations)
  
 if __name__ == "main":
-    app.run(debug=True, port='8081', host='0.0.0.0')
+    app.run(debug=True, port='5001', host='0.0.0.0')
