3391a3ac33160fdad4942e9a8dc62897c75daa62
Joel Barenco <joelbarenco@gmail.com>
2020-10-06 17:35:18 +0200

    moved to gunicorn docker image

--- app/main.py
+++ app/main.py
@@ -8,6 +8,8 @@ import logging
 
 app = Flask(__name__)
 
+logging.info("Starting up the fire..")
+
 dynamodb = boto3.resource('dynamodb')
 linksTableName = os.environ['LINKS_TABLE']
 indexName = os.environ['INDEX_NAME']
@@ -15,33 +17,26 @@ indexName = os.environ['INDEX_NAME']
 try:
   ddbTable = dynamodb.Table(linksTableName)
 except:
-  logging.debug("Error loading DynamoDB table. Check if table was created correctly and environment variable.")
+  logging.info("Error loading DynamoDB table. Check if table was created correctly and environment variable.")
 
 data = reco.loadData()
 algo = reco.initialiseEngine(data)
 
 @app.route('/reco-mvp', methods=['GET'])
 def root():
-  print('healtcheck OK')
   return "Welcome to the Flask server"
 
 @app.route('/reco-mvp/top-reco', methods=['POST'])
 def topReco():
-  try:
-    requestInput = json.loads(request.data)
+  requestInput = json.loads(request.data)
 
-    rating_dict = utils.processRequestInput(requestInput, ddbTable, indexName)
+  rating_dict = utils.processRequestInput(requestInput, ddbTable, indexName)
 
-    recommendations = reco.getReco(algo, data, rating_dict)
+  recommendations = reco.getReco(algo, data, rating_dict)
 
-    result = utils.processReco(recommendations)
+  result = utils.processReco(recommendations)
 
-    return json.dumps(result)
-  except:
-    return {
-      'StatusCode': 500,
-      'body': 'Error'
-    }
+  return json.dumps(result)
  
 if __name__ == "__main__":
     app.run(debug=True, port='5001', host='0.0.0.0')
