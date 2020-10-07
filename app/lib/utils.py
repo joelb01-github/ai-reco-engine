844aa73fd8d1f075e219e20eea789ddba4b24e6e
Joel Barenco <joelbarenco@gmail.com>
2020-10-07 18:08:41 +0200

    adding support for original imbd id in post request

--- app/lib/utils.py
+++ app/lib/utils.py
@@ -1,8 +1,9 @@
 from flask import current_app as app
+from boto3.dynamodb.conditions import Key
 import pandas as pd
 import time
 import botocore
-from boto3.dynamodb.conditions import Key
+import re
 
 def processRequestInput(requestInput, ddbTable, indexName):
   init = time.perf_counter()
@@ -11,11 +12,21 @@ def processRequestInput(requestInput, ddbTable, indexName):
 
   for row in requestInput:
     try:
+      imdbId = processImdbId(row['imdbId'])
+      row.update({'imdbId': imdbId})
+    except:
+      app.logger.error('Wrong imdbId: {}, not taking it into account.'.format(row['imdbId']))
+
+      continue
+
+    try:
       app.logger.info("searching for imdbId: {}".format(row['imdbId']))
+
       items = ddbTable.query(
         IndexName = indexName,
         KeyConditionExpression=Key('imdbId').eq(row['imdbId'])
       )['Items']
+
       app.logger.info("items fetched: {}".format(items))
     except botocore.exceptions.ClientError as error:
       app.logger.error("ClientError from dynamoDB")
@@ -24,14 +35,16 @@ def processRequestInput(requestInput, ddbTable, indexName):
 
     if len(items) > 0:
       movieId = items[0]['movieId']
+
       rating_dict.update({int(movieId): float(row['rating'])}) 
+      
       print("rating dict updated with:", movieId) 
     else:
       app.logger.error("No result found for imdbId:", row['imdbId'])
 
   app.logger.info("Successfully processed request input in: {} seconds. Rating dict contains {} elements.".format(time.perf_counter() - init, len(rating_dict)))
 
-  app.logger.info("dict:", rating_dict)
+  app.logger.info(("dict: {}").format(rating_dict))
 
   return rating_dict
 
@@ -43,6 +56,14 @@ def processReco(recommendations):
     item.update({'imdbId': updateImdbId(item['imdbId'])})
 
   return result
+
+def processImdbId(id):
+  regex = r"^tt\d{7}$"
+  
+  if re.search(regex, id):
+    return re.sub("tt", "", id)
+  else:
+    raise ValueError('imdb id format is not supported')
 
 def updateImdbId(id):
   return "tt{}".format(str(id).zfill(7))
